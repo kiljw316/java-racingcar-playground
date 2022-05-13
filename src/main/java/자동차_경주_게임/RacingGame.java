df2d4aec32517c53f10f96f74f84193b42ddbbb3
kiljaewon <kiljw316@gmail.com>
2022-05-13 09:46:34 +0900

    feat: 경주 시작 기능 및 경기 결과 반환 기능

--- src/main/java/자동차_경주_게임/RacingGame.java
+++ src/main/java/자동차_경주_게임/RacingGame.java
@@ -1,28 +1,54 @@
 package 자동차_경주_게임;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingGame {
 
     public static final int CONDITION_NUM = 4;
     private final List<Car> carList = new ArrayList<>();
     private final RandomNumber randomNumber;
+    private final ResultView resultView;
 
 
-    public RacingGame(String carNames, RandomNumber randomNumber) {
+    public RacingGame(String carNames, RandomNumber randomNumber, ResultView resultView) {
         registerRacingCars(carNames);
         this.randomNumber = randomNumber;
+        this.resultView = resultView;
     }
 
-    public void run() {
-        for (Car car : carList) {
-            carRunIfRandomNumberGreaterThanConditionNum(car);
+    public void run(int runCount) {
+        resultView.playResultInit();
+        for (int i = 0; i < runCount; i++) {
+            for (Car car : carList) {
+                carRunIfRandomNumberGreaterThanConditionNum(car);
+            }
+            resultView.playResult(carList);
         }
     }
 
     public List<Car> getCarList() {
         return this.carList;
+    }
+
+    public void result() {
+        List<String> carNames = this.carList
+                .stream()
+                .filter(
+                        car -> car.getPosition() == findMaxPosition()
+                ).map(Car::getCarName)
+                .collect(Collectors.toList());
+        resultView.finalResult(carNames);
+    }
+
+    private Integer findMaxPosition() {
+         return Collections.max(
+                this.carList
+                        .stream()
+                        .map(Car::getPosition)
+                        .collect(Collectors.toList()));
     }
 
     private void carRunIfRandomNumberGreaterThanConditionNum(Car car) {
